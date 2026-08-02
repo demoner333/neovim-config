5ee559d536a4771c3d2dae4897f31a9cace463fd
demoner333 <demoner333@gmail.com>
2026-08-02 13:28:06 +0300

    COm

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- Options for Vim --
 vim.opt.relativenumber =	true
-vim.opt.number =		true
-vim.opt.hlsearch =		true
-vim.opt.clipboard =		"unnamedplus"
-vim.opt.cursorline = 		true
-vim.opt.tabstop = 		4
+vim.opt.number =		    true
+vim.opt.hlsearch =		    true
+vim.opt.clipboard =		    "unnamedplus"
+vim.opt.cursorline = 	    true
+vim.opt.tabstop = 		    4
 vim.opt.shiftwidth = 		4
 vim.opt.softtabstop = 		4
 vim.opt.expandtab = 		true
@@ -36,24 +36,27 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Setup lazy.nvim
 require("lazy").setup({
     spec = {
-        "NvChad/nvim-colorizer.lua",
-        event = "BufReadPre",
-        opts = {
-            filetypes = { "*" },
-            user_default_options = {
-                RGB = true,          -- #RGB hex codes
-                RRGGBB = true,        -- #RRGGBB hex codes
-                names = false,       -- "Name" codes like Blue or red
-                RRGGBBAA = true,      -- #RRGGBBAA hex codes
-                AARRGGBB = true,      -- 0xAARRGGBB hex codes
-                rgb_fn = true,       -- CSS rgb() and rgba() functions
-                hsl_fn = true,       -- CSS hsl() and hsla() functions
-                css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                css_fn = true,       -- CSS v4 functional notation like rgb(0 0 0 / 0.5)
-                -- Available modes: foreground, background,  virtualtext
-                mode = "background", -- Set the display mode.
-                tailwind = true,     -- Enable tailwind colors
-                sass = { enable = true, parsers = { "css" }, }, -- Enable sass colors
+        {
+            "NvChad/nvim-colorizer.lua",
+            event = "BufReadPre",
+            opts = {
+                filetypes = { "*" },
+                user_default_options = {
+                    RGB = true,          -- #RGB hex codes
+                    RRGGBB = true,        -- #RRGGBB hex codes
+                    names = true,       -- "Name" codes like Blue or red
+                    RRGGBBAA = true,      -- #RRGGBBAA hex codes
+                    AARRGGBB = true,      -- 0xAARRGGBB hex codes
+                    rgb_fn = true,       -- CSS rgb() and rgba() functions
+                    hsl_fn = true,       -- CSS hsl() and hsla() functions
+                    hsla_fn = true,
+                    css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                    css_fn = true,       -- CSS v4 functional notation like rgb(0 0 0 / 0.5)
+                    -- Available modes: foreground, background,  virtualtext
+                    mode = "background", -- Set the display mode.
+                    tailwind = true,     -- Enable tailwind colors
+                    sass = { enable = true, parsers = { "css" }, }, -- Enable sass colors
+                },
             },
         },
         {
@@ -86,7 +89,25 @@ require("lazy").setup({
             {'akinsho/toggleterm.nvim', version = "*", config = true}
             -- or
         },
-        { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+        { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+
+        -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+        {
+            'numToStr/Comment.nvim',
+            opts = {
+                -- add any options here
+            }
+        },
+        { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
+        {
+            'nvim-telescope/telescope.nvim', version = '*',
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+                -- optional but recommended
+                { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+            }
+        }
+
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
@@ -147,10 +168,19 @@ end
 vim.keymap.set('n', '<Leader>e', toggle_nvim_tree, { desc = 'Переключить nvim-tree' })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#F0F0F0" })
 
-vim.keymap.set('n', 'J', '<CMD>tabprevious<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'H', '<CMD>tabprevious<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('n', 'L', '<CMD>tabnext<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<Leader>y', '<CMD>tabnew<CR>')
+vim.keymap.set('n', '\t', '<CMD>tabnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<S-tab>', '<CMD>tabprevious<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<Leader>tn', '<CMD>tabnew<CR>')
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {buffer = 0, silent = true})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.cmd[[colorscheme cyberdream]]
